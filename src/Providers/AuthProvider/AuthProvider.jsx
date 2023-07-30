7d6657e4bb9da817e1f0dbb93ffd6a04742bb84e
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-07-30 17:14:06 +0600

    provider

---
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,89 @@
+
+
+
+
+
+// export const AuthContext = createContext();
+// const auth = getAuth(app);
+
+// const AuthProvider = ({ children }) => {
+
+//     const [user, setUser] = useState(null);
+//     const [loading, setLoading] = useState(true);
+
+//     const googleProvider = new GoogleAuthProvider();
+
+//     const createUser = (email, password) => {
+//         setLoading(true);
+//         return createUserWithEmailAndPassword(auth, email, password);
+//     }
+
+//     const logIn = (email, password) => {
+//         setLoading(true);
+//         return signInWithEmailAndPassword(auth, email, password);
+//     }
+
+//     const logOut = () => {
+//         setLoading(true);
+//         return signOut(auth);
+//     }
+
+//     const updateUserProfile = (name, photo) => {
+//         return updateProfile(auth.currentUser, {
+//             displayName: name, photoURL: photo
+//         })
+//     }
+
+//     const googleSignIn = () => {
+//         return signInWithPopup(auth, googleProvider)
+//     }
+
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+//             setUser(currentUser);
+//             setLoading(false);
+//             if (currentUser && currentUser.email) {
+//                 const loggedUser = {
+//                     email: currentUser.email
+//                 }
+
+//                 fetch('http://localhost:5000/jwt', {
+//                     method: 'POST',
+//                     headers: {
+//                         'content-type': 'application/json'
+//                     },
+//                     body: JSON.stringify(loggedUser)
+
+//                 })
+//                     .then(res => res.json())
+//                     .then(data => {
+//                         console.log('jwt response', data)
+//                         localStorage.setItem('data-access-token', data.token)
+//                     })
+
+//             }
+
+//             else {
+//                 localStorage.removeItem('data-access-token')
+//             }
+
+
+//         })
+//         return () => {
+//             unsubscribe();
+//         }
+//     }, [])
+
+//     const authInfo = { user, loading, createUser, logIn, logOut, updateProfile, googleSignIn, updateUserProfile }
+
+//     return (
+//         <div>
+//             <AuthContext.Provider value={authInfo}>
+//                 {children}
+//             </AuthContext.Provider>
+//         </div>
+//     );
+
+// };
+
+// export default AuthProvider;
