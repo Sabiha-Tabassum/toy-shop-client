257c4ea84dc7f0df02854b6b5f5bd96bc9865eba
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-07-30 16:19:41 +0600

    slider

--- src/Components/CategoryData/CategoryData.jsx
+++ src/Components/CategoryData/CategoryData.jsx
@@ -1,9 +1,16 @@
 
 
-const CategoryData = () => {
+const CategoryData = ({item}) => {
+    const { name, image, price } = item
     return (
-        <div>
-            
+        <div className="max-w-6xl mx-auto">
+            <div className="card bg-slate-200  card-compact w-72 h-56 mb-4 shadow-xl">
+                <figure><img className="w-48 h-full mt-4" src={image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>Price: {price}</p>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,51 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+import slider1 from '../../images/pink-easter-car-with-red-egg.jpg'
+import slider2 from '../../images/black-friday-elements-assortment.jpg'
+import slider3 from '../../images/slider2.jpg'
+import slider4 from '../../images/slider.jpg'
+import slider5 from '../../images/hand-toy-car-my-hand-1253586.jpg'
+
+const Slider = () => {
+    return (
+        <div>
+            <div className='text-center my-24'>
+                <p className='font-semibold'>_ _ _ _From 8 A.M to 7 P.M_ _ _ _</p>
+                <h1 className='text-amber-400 font-bold text-2xl'>Order Online</h1>
+            </div>
+            <div className='max-w-6xl mx-auto'>
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={15}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img className='h-64' src={slider1} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img className='h-64' src={slider2} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img className='h-64' src={slider3} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slider4} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slider5} alt="" />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
