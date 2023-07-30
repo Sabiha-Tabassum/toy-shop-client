0fb0438f85ee288328d596caf9f59d3e35884c27
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-07-30 16:56:55 +0600

    login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,126 @@
 
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+import { FaGoogle } from 'react-icons/fa';
+
+
+import { useState } from "react";
+
 
 const Login = () => {
+
+    // const { logIn, updateProfile, googleSignIn } = useContext(AuthContext);
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/'
+
+
+
+    const handleLoginForm = event => {
+        event.preventDefault();
+        const form = event.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        logIn(email, password)
+            .then(result => {
+                const user = result.user;
+
+                console.log(user);
+
+                setError('');
+
+                event.target.reset();
+                navigate(from, { replace: true })
+            })
+
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+
+            })
+
+        updateProfile();
+
+    }
+
+    // googleSignIn
+
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(result => {
+                const googleUser = result.user;
+                console.log(googleUser);
+                navigate(from, { replace: true })
+
+            })
+
+            .catch(error => {
+                console.log('error', error.message);
+
+            })
+
+    }
+
     return (
-        <div>
-            
+        <div className='max-w-6xl mx-auto mb-2'>
+            <div className="hero min-h-screen  bg-slate-100 ">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+
+                       
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <h1 className="text-3xl font-bold ">Login now!</h1>
+                            <form onSubmit={handleLoginForm}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold">Email</span>
+                                    </label>
+                                    <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold">Password</span>
+                                    </label>
+                                    <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+
+                                </div>
+
+                                <div className="form-control mt-6">
+
+                                    <input className="btn btn-outline border-b-4 border-slate-700" type="submit" value="Login" />
+                                </div>
+                            </form>
+                            <div>
+                                <p className='text-red-500'>{error}</p>
+                            </div>
+
+                            <br />
+                            <div>
+                                <p>Doesn't have an Account yet? <Link className='text-slate-700-500 font-bold' to="/signup">Sign Up</Link></p>
+                            </div>
+                            <br />
+                            <div>
+                                <p className='text-slate-500 font-semibold'>_____________________OR_________________________</p>
+                            </div>
+                            <div className=' ml-24'>
+                                <button onClick={handleGoogleSignIn} className="btn btn-outline  w-28">  <FaGoogle className='text-slate-700'></FaGoogle></button>
+                            </div>
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/MyToy/MyToy.jsx
+++ src/Pages/MyToy/MyToy.jsx
@@ -1,9 +1,95 @@
+import {  useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import MyToyData from "./MyToyData";
 
 
 const MyToy = () => {
+    // const { user } = useContext(AuthContext);
+    const [myToys, setMyToys] = useState([]);
+    const navigate = useNavigate()
+
+    const url = `http://localhost:5000/addToy?email=${user?.email}`
+    useEffect(() => {
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('data-access-token')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (!data.error) {
+                    setMyToys(data)
+                }
+                else {
+                    navigate('/')
+                }
+            })
+
+    }, [url, navigate])
+
+    const handleDelete = (id) => {
+        const proceed = confirm('Are you sure you want to delete')
+        if (proceed) {
+            fetch(`http://localhost:5000/addToy/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Your data has been deleted successfully',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+
+                        const remaining = myToys.filter(myToy => myToy._id !== id)
+                        setMyToys(remaining);
+                    }
+
+                })
+        }
+    }
+
     return (
-        <div>
-            
+        <div className='max-w-5xl mx-auto'>
+            <div className="overflow-x-auto w-full">
+                <div>{myToys.length}</div>
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Email</th>
+
+                            <th>Available Quantity</th>
+                            <th>Price</th>
+                            <th>deadline</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myToys.map(MyToy => <MyToyData
+                                key={MyToy._id}
+                                MyToy={MyToy}
+                                handleDelete={handleDelete}
+
+                            >
+
+                            </MyToyData>)
+                        }
+
+
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
