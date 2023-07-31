d1d1573b607452dde17aeaa777b1f1eb24240eaf
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-07-31 10:02:12 +0600

    added

--- src/Providers/PrivateRoute/PrivateRoute.jsx
+++ src/Providers/PrivateRoute/PrivateRoute.jsx
@@ -1,20 +1,22 @@
-// import { Navigate, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
-// const PrivateRoute = () => {
-//     const {user,loading} = useContext(AuthContext);
+const PrivateRoute = ({children}) => {
+    const {user,loading} = useContext(AuthContext);
 
-//     const location = useLocation();
-//     console.log(location);
+    const location = useLocation();
+    console.log(location);
 
-//    if(loading){
-//     return <progress className="progress w-56"></progress>
-//    }
+   if(loading){
+    return <progress className="progress w-56"></progress>
+   }
 
-//    if(user?.email){
-//     return children
-//    }
+   if(user?.email){
+    return children
+   }
 
-//    return <Navigate state={{from: location}} to= '/login' replace></Navigate>
-// };
+   return <Navigate state={{from: location}} to= '/login' replace></Navigate>
+};
 
-// export default PrivateRoute;
+export default PrivateRoute;
