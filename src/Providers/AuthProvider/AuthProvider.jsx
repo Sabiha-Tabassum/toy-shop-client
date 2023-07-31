e133b3a904a6f6f66291722745b02a399e93f00a
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-07-31 09:56:23 +0600

    added

--- src/Providers/AuthProvider/AuthProvider.jsx
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -1,89 +1,92 @@
+import { createContext, useEffect, useState } from "react";
+import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from "firebase/auth"
+import app from "../../firebase/firebase.config";
 
 
 
 
 
-// export const AuthContext = createContext();
-// const auth = getAuth(app);
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
-// const AuthProvider = ({ children }) => {
+const AuthProvider = ({ children }) => {
 
-//     const [user, setUser] = useState(null);
-//     const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-//     const googleProvider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
 
-//     const createUser = (email, password) => {
-//         setLoading(true);
-//         return createUserWithEmailAndPassword(auth, email, password);
-//     }
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
-//     const logIn = (email, password) => {
-//         setLoading(true);
-//         return signInWithEmailAndPassword(auth, email, password);
-//     }
+    const logIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
-//     const logOut = () => {
-//         setLoading(true);
-//         return signOut(auth);
-//     }
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
 
-//     const updateUserProfile = (name, photo) => {
-//         return updateProfile(auth.currentUser, {
-//             displayName: name, photoURL: photo
-//         })
-//     }
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        })
+    }
 
-//     const googleSignIn = () => {
-//         return signInWithPopup(auth, googleProvider)
-//     }
+    const googleSignIn = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
 
-//     useEffect(() => {
-//         const unsubscribe = onAuthStateChanged(auth, currentUser => {
-//             setUser(currentUser);
-//             setLoading(false);
-//             if (currentUser && currentUser.email) {
-//                 const loggedUser = {
-//                     email: currentUser.email
-//                 }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+            if (currentUser && currentUser.email) {
+                const loggedUser = {
+                    email: currentUser.email
+                }
 
-//                 fetch('http://localhost:5000/jwt', {
-//                     method: 'POST',
-//                     headers: {
-//                         'content-type': 'application/json'
-//                     },
-//                     body: JSON.stringify(loggedUser)
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(loggedUser)
 
-//                 })
-//                     .then(res => res.json())
-//                     .then(data => {
-//                         console.log('jwt response', data)
-//                         localStorage.setItem('data-access-token', data.token)
-//                     })
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('jwt response', data)
+                        localStorage.setItem('data-access-token', data.token)
+                    })
 
-//             }
+            }
 
-//             else {
-//                 localStorage.removeItem('data-access-token')
-//             }
+            else {
+                localStorage.removeItem('data-access-token')
+            }
 
 
-//         })
-//         return () => {
-//             unsubscribe();
-//         }
-//     }, [])
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
 
-//     const authInfo = { user, loading, createUser, logIn, logOut, updateProfile, googleSignIn, updateUserProfile }
+    const authInfo = { user, loading, createUser, logIn, logOut, updateProfile, googleSignIn, updateUserProfile }
 
-//     return (
-//         <div>
-//             <AuthContext.Provider value={authInfo}>
-//                 {children}
-//             </AuthContext.Provider>
-//         </div>
-//     );
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
 
-// };
+};
 
-// export default AuthProvider;
+export default AuthProvider;
